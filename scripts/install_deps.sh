0ab55f41e45c83e66f0d5598adea8df2a0b80cd0
welfi <welfi.kharroubi@esprit.tn>
2023-09-24 04:35:04 +0100

    azfff

--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -1,22 +1,35 @@
 #!/bin/bash
 
-# Update the package manager and install TeX Live
-yum update -y
-yum install texlive texlive-latex -y
-yum install texlive-collection-basic -y
+# Define the directory where TeX Live will be installed
+TEXLIVE_INSTALL_DIR="/usr/local/texlive"
 
-TEXLIVE_PATH=$(dirname $(which pdflatex))
+# Define the profile to use for installation (scheme-basic, scheme-medium, etc.)
+INSTALL_PROFILE="scheme-medium"
+
+# Download the TeX Live installer script
+curl -sO https://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
+
+# Extract the installer
+tar -xzf install-tl-unx.tar.gz
+cd install-tl-*
+
+# Run the TeX Live installer with the specified profile and installation directory
+./install-tl -profile=/dev/stdin <<EOF
+selected_scheme $INSTALL_PROFILE
+TEXDIR $TEXLIVE_INSTALL_DIR
+EOF
+
+# Add TeX Live binaries to the system's PATH
+echo "export PATH=\"$TEXLIVE_INSTALL_DIR/bin/x86_64-linux:\$PATH\"" >> ~/.bashrc
+source ~/.bashrc  # Refresh the shell environment
 
 # Print TeX Live version
-echo "pdflatex --version"
 pdflatex --version
-echo "tex --version"
 
-
-# Use tlmgr with the full path
-echo "Using tlmgr from its installation path:"
-TLMPGR_PATH="$TEXLIVE_PATH/tlmgr"
-
-# Use tlmgr with the full path
-echo "Using tlmgr from its installation path:"
+# Check the version of tlmgr
 tlmgr --version
+
+# Optionally, install additional TeX Live packages as needed
+# tlmgr install <package-name>
+
+echo "TeX Live installation complete."
