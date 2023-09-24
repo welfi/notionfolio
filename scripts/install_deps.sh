2a54e436f249a538cf3c2b5decbdf4dd49fb74df
welfi <welfi.kharroubi@esprit.tn>
2023-09-24 03:22:24 +0100

    new test script

---
+++ scripts/install_deps.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Update the package manager and install TeX Live
+yum update -y
+yum install texlive texlive-latex -y
+
+# Print TeX Live version
+pdflatex --version
+
+# Optionally, install additional TeX Live packages as needed
+# sudo yum install texlive-<package-name> -y
