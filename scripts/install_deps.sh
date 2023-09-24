0975133e046443be395b3b6814f1763e9da4787f
welfi <welfi.kharroubi@esprit.tn>
2023-09-24 03:46:55 +0100

    vv

--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -4,12 +4,17 @@
 yum update -y
 yum install texlive texlive-latex -y
 
+# Determine the TeX Live installation path
+TEXLIVE_PATH=$(dirname $(which pdflatex))
+
 # Print TeX Live version
 echo "pdflatex --version"
 pdflatex --version
 echo "tex --version"
-tex --version
-echo "which tlmgr"
-which tlmgr
+
+# Export the TeX Live path to the PATH environment variable
+export PATH="$TEXLIVE_PATH:$PATH"
+echo "tlmgr path"
+tlmgr --version
 # Optionally, install additional TeX Live packages as needed
 # sudo yum install texlive-<package-name> -y
